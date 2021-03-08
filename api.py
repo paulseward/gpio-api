ae81df72081c144897d64ede15055c04e6dac281
Paul Seward-Prior <paul@paulseward.com>
2021-03-08 17:15:00 +0000

    document root serves a minimal bootstrap table showing the current state

--- api.py
+++ api.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
-from bottle import route, run
+from bottle import route, run, static_file
 import RPi.GPIO as GPIO
 import yaml
 
 # API routing
 @route('/')
 def apiRoot():
-  return "Hello"
+  return static_file('index.html', root='.')
 
 @route('/json')
 def apiJson():
-  readPins(pinMap)
-  return {"gpio":pinMap}
+  return {"gpio": readPins(config["pinMap"])}
 
 # GPIO processing
 def readPins(pinMap):
@@ -25,17 +24,17 @@ def main():
   """
 
   # Read our pinmap definition file, make it available globally
-  with open('pinmap.yaml') as f:
-    global pinMap
-    pinMap = yaml.safe_load(f)
+  with open('config.yaml') as f:
+    global config
+    config = yaml.safe_load(f)
 
   # Set's GPIO pins to BCM GPIO numbering, and input mode
   GPIO.setmode(GPIO.BCM)
-  for pin in pinMap:
+  for pin in config["pinMap"]:
     GPIO.setup(pin["pin"], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
   # Start the bottle API server
-  run(host='0.0.0.0', port=8080, debug=True)
+  run(host=config["listen"]["host"], port=config["listen"]["port"], debug=True)
 
 if __name__ == "__main__":
   main()
